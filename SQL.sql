6c7180c6d32ab37d88d0480495c9c0ee70f371cf
bit-user <bit-user@DESKTOP-C1QR4BV>
2017-08-22 13:55:15 +0900

    HRSalary

--- SQL.sql
+++ SQL.sql
@@ -1,3 +1,12 @@
+select employee_id, 
+       first_name, 
+	   last_name, 
+	   salary
+ from employees;
+
+-------------------------------------------------
+
+
 select * from author;
 
 update author set name='도우넛' where no = 12;
