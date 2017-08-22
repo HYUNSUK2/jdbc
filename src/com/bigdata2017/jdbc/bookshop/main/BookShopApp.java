abfef9f27831510957b1d661327233bd1bfe6e99
bit-user <bit-user@DESKTOP-C1QR4BV>
2017-08-22 15:45:25 +0900

    main 수정

--- src/com/bigdata2017/jdbc/bookshop/main/BookShopApp.java
+++ src/com/bigdata2017/jdbc/bookshop/main/BookShopApp.java
@@ -5,21 +5,33 @@ import java.util.Scanner;
 public class BookShopApp {
 
 	public static void main(String[] args) {
-		//Book 객체의 정보를 출력
-		System.out.println("*****도서 정보 출력하기******");
-		displayBookInfo();
-		
+
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("대여 하고 싶은 책의 번호를 입력하세요:");
-		int num = scanner.nextInt();
+
+		while( true ) {
+			System.out.println("*****도서 정보 출력하기******");
+			displayBookInfo();
+			
+			System.out.print("대여 하고 싶은 책의 번호를 입력하세요:");
+			String input = scanner.nextLine();
+			
+			// quit이면 종료
+			if( "quit".equals( input.toLowerCase() ) ) {
+				break;
+			}
+			
+			// 숫자가 아님
+			if( input.matches("\\d+") == false ) {
+				continue;
+			}
+			
+			Long no = Long.parseLong( input );
+			System.out.println( no );
+			
+			/*  state 정보 setting */
+		}
+		
 		scanner.close();
-		
-		/*  state 정보 setting */
-		
-		
-		//Book 객체의 정보를 출력
-		System.out.println("*****도서 정보 출력하기******");
-		displayBookInfo();		
 	}
 	
 	private static void displayBookInfo() {
